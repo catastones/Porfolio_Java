b7418cecbb6dd664a4b32de5b15b36e23163ea7a
Cristian Alderete <catastones@gmail.com>
2022-07-30 19:28:54 -0300

    cross global

--- src/main/java/com/Porfolio2/App/AppApplication.java
+++ src/main/java/com/Porfolio2/App/AppApplication.java
@@ -3,12 +3,15 @@ package com.Porfolio2.App;
 import ControladorSeguridad.JWTAuthorizationFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AppApplication {
@@ -31,5 +34,14 @@ public class AppApplication {
                                         .antMatchers("/api/tiposempleos").permitAll() 
 					.anyRequest().authenticated();
 		}
+	}
+
+@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("https://porfoliocristian-ed2f1.web.app/").allowedMethods("*").allowedHeaders("*");
+			}
+		};
 	}
 }
